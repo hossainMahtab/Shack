ce08544baf4f77d323b88cbcb79c9a8b1b0d75db
Mahtab-TalentPro <mahtabtalentpro@gmail.com>
2023-11-11 14:05:27 +0600

    feat: signin form

--- src/components/login/SignIn.js
+++ src/components/login/SignIn.js
@@ -2,12 +2,16 @@ import {
   Checkbox,
   FormControlLabel,
   FormGroup,
+  IconButton,
+  InputAdornment,
   TextField,
 } from "@mui/material";
 import { AiOutlineClose } from "react-icons/ai";
-import React from "react";
+import React, { useState } from "react";
 import CommonModal from "../shared/modal/CommonModal";
 import SignUp from "./SignUp";
+import { useForm } from "react-hook-form";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const SignIn = (props) => {
   const handleClose = () => {
@@ -17,16 +21,37 @@ const SignIn = (props) => {
     props.handleSignUpOpen();
   };
 
+  // form submission
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignInSubmit = (data) => {
+    console.log("Form data:", data);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
-      <form className="w-full h-full relative z-[1111]">
+      <form
+        onSubmit={handleSubmit(handleSignInSubmit)}
+        className="w-full h-full relative z-[1111]"
+      >
         <div className="absolute top-5 right-5">
-          <button
+          <div
             onClick={handleClose}
             className="bg-transparent text-white font-bold p-2  hover:rounded-full hover:bg-white hover:text-[#e4ae62]"
           >
             <AiOutlineClose size={20} />
-          </button>
+          </div>
         </div>
         <div className="flex flex-col items-center justify-center h-full py-10 px-12 lg:px-16 xl:px-20">
           <div className="flex flex-col gap-4 items-center justify-center text-center">
@@ -39,12 +64,13 @@ const SignIn = (props) => {
               Sign in to your account
             </h2>
           </div>
-          <div className="w-full flex flex-col items-center justify-center mt-6">
-            <div className="w-full flex flex-col items-center justify-center">
+          <div className="w-full flex flex-col gap-4 items-center justify-center mt-6">
+            <div className="w-full flex flex-col gap-2 items-center justify-center">
               <TextField
+                // required
                 fullWidth
                 type="email"
-                className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
+                className="!w-full !h-full !rounded-md custom-mui-input !bg-inherit !font-bold"
                 sx={{
                   "& label.Mui-focused": {
                     color: "white",
@@ -65,24 +91,41 @@ const SignIn = (props) => {
                     },
                   },
                 }}
-                name="email"
+                // name="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Invalid email address",
+                  },
+                })}
                 label="Email"
-                id="outlined-basic"
+                // id="outlined-basic"
                 variant="outlined"
               />
+              {errors.email && (
+                <p className="text-[#e4ae62]">{errors.email.message}</p>
+              )}
             </div>
-            <div className="w-full flex flex-col items-center justify-center mt-4">
-              <div className="w-full flex flex-col items-center justify-center">
+            <div className="w-full flex flex-col gap-2 items-center justify-center">
+              <div className="w-full flex flex-col gap-2 items-center justify-center">
                 <TextField
+                  // required
                   fullWidth
-                  type="password"
-                  className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
+                  type={showPassword ? "text" : "password"}
+                  className="!w-full !h-full !rounded-md custom-mui-input !bg-inherit !font-bold"
                   sx={{
                     "& label.Mui-focused": {
                       color: "white",
                     },
                     "& .MuiInputLabel-root": {
                       color: "white",
+                    },
+
+                    "& .MuiInputBase-root": {
+                      "& input": {
+                        color: "white",
+                      },
                     },
 
                     "& .MuiOutlinedInput-root": {
@@ -97,13 +140,32 @@ const SignIn = (props) => {
                       },
                     },
                   }}
-                  name="password"
+                  // name="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
                   label="Password"
-                  id="outlined-basic"
+                  // id="outlined-basic"
                   variant="outlined"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          className="!text-white"
+                          onClick={handleTogglePassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
+                {errors.password && (
+                  <p className="text-[#e4ae62]">{errors.password.message}</p>
+                )}
               </div>
-              <div className="w-full flex  items-center justify-end mt-4">
+              <div className="w-full flex  items-center justify-end ">
                 {/* <FormGroup className="!text-xs lg:!text-sm !text-left !hidden">
                 <FormControlLabel
                   sx={{
@@ -129,20 +191,23 @@ const SignIn = (props) => {
             </div>
           </div>
           <div className="w-full flex flex-col items-center justify-center mt-6">
-            <button className="w-full py-3.5  rounded-md bg-[#e4ae62] text-white font-bold cursor-pointer">
+            <button
+              type="submit"
+              className="w-full py-3.5  rounded-md bg-[#e4ae62] text-white font-bold cursor-pointer"
+            >
               Sign In
             </button>
           </div>
-          <div className="flex flex-col items-center justify-center text-center mt-6">
+          <div className="flex flex-wrap gap-0.5 lg:gap-1 items-center justify-center text-center mt-6">
             <h2 className="text-xl text-white font-bold">
               Don't have an account?{" "}
-              <button
-                onClick={handleSignUp}
-                className="bg-transparent  text-[#e4ae62] cursor-pointer"
-              >
-                Sign Up
-              </button>
             </h2>
+            <p
+              onClick={handleSignUp}
+              className="bg-transparent text-xl font-bold   text-[#e4ae62] cursor-pointer"
+            >
+              Sign Up
+            </p>
           </div>
         </div>
       </form>
--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,5 +1,7 @@
-import { TextField } from "@mui/material";
-import React from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { AiOutlineClose } from "react-icons/ai";
 
 const SignUp = (props) => {
@@ -11,15 +13,38 @@ const SignUp = (props) => {
   const handleSignIn = () => {
     props.handleSignInOpen();
   };
+
+  //signup form submission
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const password = watch("password", "");
+
+  const handleSignUpSubmit = (data) => {
+    console.log("FormData:", data);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <form className="w-full h-full relative ">
+    <form
+      className="w-full h-full relative "
+      onSubmit={handleSubmit(handleSignUpSubmit)}
+    >
       <div className="absolute top-5 right-5">
-        <button
+        <div
           onClick={handleClose}
           className="bg-transparent text-white font-bold p-2  hover:rounded-full hover:bg-white hover:text-[#e4ae62]"
         >
           <AiOutlineClose size={20} />
-        </button>
+        </div>
       </div>
       <div className="flex flex-col items-center justify-center h-full py-10 px-12 lg:px-16 xl:px-20">
         <div className="flex flex-col gap-4 items-center justify-center text-center">
@@ -32,12 +57,12 @@ const SignUp = (props) => {
             Sign Up to your account
           </h2>
         </div>
-        <div className="w-full flex flex-col items-center justify-center mt-6">
-          <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-full flex flex-col gap-4 items-center justify-center mt-6">
+          <div className="w-full flex flex-col gap-2 items-center justify-center">
             <TextField
               fullWidth
               type="text"
-              className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
+              className="!w-full !h-full rounded-md custom-mui-input !bg-inherit !font-bold"
               sx={{
                 "& label.Mui-focused": {
                   color: "white",
@@ -58,17 +83,22 @@ const SignUp = (props) => {
                   },
                 },
               }}
-              name="name"
+              // name="name"
+              {...register("name", {
+                required: "Name is required",
+              })}
               label="Name"
-              id="outlined-basic"
               variant="outlined"
             />
+            {errors.name && (
+              <p className="text-[#e4ae62]">{errors.name.message}</p>
+            )}
           </div>
-          <div className="w-full flex flex-col items-center justify-center mt-4">
+          <div className="w-full flex flex-col gap-2 items-center justify-center">
             <TextField
               fullWidth
               type="email"
-              className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
+              className="!w-full !h-full rounded-md custom-mui-input !bg-inherit !font-bold"
               sx={{
                 "& label.Mui-focused": {
                   color: "white",
@@ -89,94 +119,140 @@ const SignUp = (props) => {
                   },
                 },
               }}
-              name="email"
+              // name="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Invalid email address",
+                },
+              })}
               label="Email"
-              id="outlined-basic"
               variant="outlined"
             />
+            {errors.email && (
+              <p className="text-[#e4ae62]">{errors.email.message}</p>
+            )}
           </div>
-          <div className="w-full flex flex-col items-center justify-center mt-4">
-            <div className="w-full flex flex-col items-center justify-center">
-              <TextField
-                fullWidth
-                type="password"
-                className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
-                sx={{
-                  "& label.Mui-focused": {
-                    color: "white",
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "white",
-                  },
+          <div className="w-full flex flex-col gap-2 items-center justify-center">
+            <TextField
+              fullWidth
+              type="password"
+              className="!w-full !h-full rounded-md custom-mui-input  !bg-inherit !font-bold"
+              sx={{
+                "& label.Mui-focused": {
+                  color: "white",
+                },
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                },
 
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "white",
-                    },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "white",
-                    },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "white",
                   },
-                }}
-                name="password"
-                label="Password"
-                id="outlined-basic"
-                variant="outlined"
-              />
-            </div>
+                  "&:hover fieldset": {
+                    borderColor: "white",
+                  },
+                  "&.Mui-focused fieldset": {
+                    borderColor: "white",
+                  },
+                },
+              }}
+              // name="password"
+              {...register("password", {
+                required: "Password is required",
+              })}
+              label="Password"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      className="!text-white"
+                      onClick={handleTogglePassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            {errors.password && (
+              <p className="text-[#e4ae62]">{errors.password.message}</p>
+            )}
           </div>
-          <div className="w-full flex flex-col items-center justify-center mt-4">
-            <div className="w-full flex flex-col items-center justify-center">
-              <TextField
-                fullWidth
-                type="password"
-                className="!w-full !h-full rounded-md  !bg-inherit !font-bold"
-                sx={{
-                  "& label.Mui-focused": {
-                    color: "white",
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "white",
-                  },
+          <div className="w-full flex flex-col gap-2 items-center justify-center">
+            <TextField
+              fullWidth
+              type="password"
+              className="!w-full !h-full rounded-md custom-mui-input  !bg-inherit !font-bold"
+              sx={{
+                "& label.Mui-focused": {
+                  color: "white",
+                },
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                },
 
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "white",
-                    },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "white",
-                    },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "white",
                   },
-                }}
-                name="confirm password"
-                label="Confirm Password"
-                id="outlined-basic"
-                variant="outlined"
-              />
-            </div>
+                  "&:hover fieldset": {
+                    borderColor: "white",
+                  },
+                  "&.Mui-focused fieldset": {
+                    borderColor: "white",
+                  },
+                },
+              }}
+              // name="confirm password"
+              {...register("confirmpass", {
+                required: "Confirm Password is required",
+                validate: (value) =>
+                  value === password || "The passwords do not match",
+              })}
+              label="Confirm Password"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      className="!text-white"
+                      onClick={handleTogglePassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            {errors.confirmpass && (
+              <p className="text-[#e4ae62]">{errors.confirmpass.message}</p>
+            )}
           </div>
         </div>
         <div className="w-full flex flex-col items-center justify-center mt-6">
-          <button className="w-full py-3.5  rounded-md bg-[#e4ae62] text-white font-bold cursor-pointer">
+          <button
+            type="submit"
+            className="w-full py-3.5  rounded-md bg-[#e4ae62] text-white font-bold cursor-pointer"
+          >
             Sign Up
           </button>
         </div>
-        <div className="flex flex-col items-center justify-center text-center mt-6">
+        <div className="flex flex-wrap gap-0.5 lg:gap-1 items-center justify-center text-center mt-6">
           <h2 className="text-xl text-white font-bold">
             Already have an account?{" "}
-            <button
-              onClick={handleSignIn}
-              className="bg-transparent  text-[#e4ae62] cursor-pointer"
-            >
-              Sign In
-            </button>
           </h2>
+          <p
+            onClick={handleSignIn}
+            className="bg-transparent text-xl font-bold   text-[#e4ae62] cursor-pointer"
+          >
+            Sign In
+          </p>
         </div>
       </div>
     </form>
